c02857401028aa89a911be372e83941d6729da14
Sanjay Singh <sanjay007abd@gmail.com>
2024-08-06 23:10:38 +0530

    full screen functionality done

--- script.js
+++ script.js
@@ -77,13 +77,69 @@ function setVolumeBar(e){
     lastVolume = volume
 
 }
+function toggleMute(){
+    volumeIcon.className =''
+    if (video.volume){
+        lastVolume = video.volume
+        video.volume = 0
+        volumeBar.style.width = 0
+        volumeIcon.classList.add('fas','fa-volume-mute');
+        volumeIcon.setAttribute('title', 'Unmute')
+    }else{
+        video.volume = lastVolume
+        volumeBar.style.width = `${lastVolume * 100}%`;
+        volumeIcon.classList.add('fas','fa-volume-up');
+        volumeIcon.setAttribute('title', 'Mute')
+    }
+}
 
 
 // Change Playback Speed -------------------- //
+function changeSpeed(){
+    video.playbackRate = speed.value
+}
 
 
 
 // Fullscreen ------------------------------- //
+
+function openFullscreen(elem) {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+      elem.msRequestFullscreen();
+    }
+    video.classList.add('video-fullscreen')
+  }
+  
+  /* Close fullscreen */
+  function closeFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+      document.msExitFullscreen();
+    }
+    video.classList.remove('video-fullscreen')
+  }
+
+let fullscreen = false
+function toggleFullScreen(){
+    if (!fullscreen){
+        openFullscreen(player)
+    }else{
+        closeFullscreen()
+    }
+    fullscreen = !fullscreen
+
+}
+
+
+
+
 
 
 // add Event Listner
@@ -92,3 +148,6 @@ video.addEventListener('click',togglePlay)
 video.addEventListener('timeupdate',updateProgressBar)
 ProgressRange.addEventListener('click',setProgressBar)
 volumeRange.addEventListener('click',setVolumeBar)
+volumeIcon.addEventListener('click',toggleMute)
+speed.addEventListener('change',changeSpeed)
+fullscreenBtn.addEventListener('click',toggleFullScreen)
